99e201f9d76f21cf38eb3c31b7fa321eeeafbab3
Raju Athokpam Singh <bdyats@bankdata.dk>
2017-03-27 17:24:56 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from "./heroes.component";
+import { HeroService } from "./hero.service";
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+
+@NgModule({
+  imports: [ 
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule     
+  ],
+  declarations: [ 
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent 
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [ HeroService ]
+})
+
+export class AppModule {
+
+}
+
+
---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { ActivatedRoute, Params } from "@angular/router";
+import { HeroService } from "./hero.service";
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-hero-detail',
+    template: `
+        <div *ngIf="hero">
+            <h2>{{hero.name}} details!</h2>
+            <div><label>id: </label>{{hero.id}}<div>
+            <div>
+            <label>name: </label>    
+            <input [(ngModel)]="hero.name" placeholder="name">
+            <div>
+            <button (click)="goBack()">Back</button>
+        </div>
+    `,
+    styleUrls:[  './hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {        
+
+    @Input()
+    hero: Hero;
+
+    constructor(
+        private heroService: HeroService,
+        private route: ActivatedRoute,
+        private location: Location
+    ){}
+
+    ngOnInit(): void {
+        this.route.params.switchMap(
+            (params: Params) => this.heroService.getHero(+params['id']))
+                .subscribe(hero => this.hero = hero);        
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+}
+
